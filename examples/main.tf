0edfe8d8c8bc7b5c0b1729c004bef70590ec54c7
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-06-21 16:58:04 -0300

    chore: reduce subnets on example

--- examples/main.tf
+++ examples/main.tf
@@ -7,9 +7,9 @@ module "my_ec2" {
   vpc_name = "my_vpc"
   vpc_cidr = "10.0.0.0/16"
   # VPC Subnets
-  intra_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  private_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+  intra_subnets   = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+  private_subnets = ["10.0.201.0/24"]
   # Network ACLs
   intra_inbound_acl_rules = {
     "mongodb" = {
