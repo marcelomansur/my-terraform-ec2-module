671846c87187eb26496d601b0370f80aff3277f1
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-06-21 16:57:23 -0300

    feat: implements outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "webserver_public_ip" {
   description = "EC2 instance public IP"
-  value       = module.my_ec2.webserver_public_ip
+  value       = aws_instance.my_ec2_webserver_cluster.public_ip
 }
 
 output "webserver_private_ip" {
   description = "EC2 instance private IP"
-  value       = module.my_ec2.webserver_private_ip
+  value       = aws_instance.my_ec2_webserver_cluster.private_ip
 }
 
 output "monitoring_private_ip" {
   description = "EC2 instance private IP"
-  value       = module.my_ec2.monitoring_private_ip
+  value       = aws_instance.my_ec2_monitoring_cluster.private_ip
 }
 
 output "database_private_ip" {
   description = "EC2 instance database IP"
-  value       = module.my_ec2.database_private_ip
+  value       = aws_instance.my_ec2_database_cluster.private_ip
 }
