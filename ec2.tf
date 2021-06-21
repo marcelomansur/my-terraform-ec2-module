bc83c5eaa524fbbd49d7f9d463d539ae9eb3d874
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-06-21 16:58:38 -0300

    feat: implements monitoring and database clusters

--- ec2.tf
+++ ec2.tf
@@ -13,10 +13,11 @@ data "aws_ami" "packer_ami" {
 # for an instance
 resource "random_integer" "subnet_index" {
   min = 0
-  max = length(var.public_subnets) - 1
+  max = length(var.public_subnets) > 1 ? length(var.public_subnets) - 1 : 1
 }
 
-resource "aws_instance" "my_ec2_web_server_cluster" {
+# Web servers
+resource "aws_instance" "my_ec2_webserver_cluster" {
   ami           = data.aws_ami.packer_ami.id
   key_name      = aws_key_pair.my_key_pair.key_name
   instance_type = "t2.micro"
@@ -31,7 +32,49 @@ resource "aws_instance" "my_ec2_web_server_cluster" {
   associate_public_ip_address = true
 
   tags = {
-    Name   = "my_ec2_web_server_cluster"
+    Name   = "my_ec2_webserver_cluster"
+    Deploy = "Terraform"
+  }
+
+  depends_on = [aws_key_pair.my_key_pair]
+}
+
+# Monitoring servers
+resource "aws_instance" "my_ec2_monitoring_cluster" {
+  ami           = data.aws_ami.packer_ami.id
+  key_name      = aws_key_pair.my_key_pair.key_name
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.my_private_sg.id]
+
+  subnet_id = lookup(aws_subnet.my_private_subnet,
+    var.private_subnets[random_integer.subnet_index.result],
+    var.private_subnets[0]
+  ).id
+
+  tags = {
+    Name   = "my_ec2_monitoring_cluster"
+    Deploy = "Terraform"
+  }
+
+  depends_on = [aws_key_pair.my_key_pair]
+}
+
+# Database servers
+resource "aws_instance" "my_ec2_database_cluster" {
+  ami           = data.aws_ami.packer_ami.id
+  key_name      = aws_key_pair.my_key_pair.key_name
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.my_intra_sg.id]
+
+  subnet_id = lookup(aws_subnet.my_intra_subnet,
+    var.intra_subnets[random_integer.subnet_index.result],
+    var.intra_subnets[0]
+  ).id
+
+  tags = {
+    Name   = "my_ec2_database_cluster"
     Deploy = "Terraform"
   }
 
