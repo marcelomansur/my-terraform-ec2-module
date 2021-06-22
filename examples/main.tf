e1916316b83f2db305e8e2447f87a9a91ec4c530
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-06-22 11:29:57 -0300

    chore: remove needless double quotes

--- examples/main.tf
+++ examples/main.tf
@@ -11,48 +11,48 @@ module "my_ec2" {
   private_subnets = ["10.0.201.0/24"]
   # Network ACLs
   intra_inbound_acl_rules = {
-    "mongodb" = {
-      "rule_number" = 110
-      "rule_action" = "allow"
-      "from_port"   = 27017
-      "to_port"     = 27017
-      "protocol"    = "tcp"
-      "cidr_block"  = "0.0.0.0/0"
+    mongodb = {
+      rule_number = 110
+      rule_action = "allow"
+      from_port   = 27017
+      to_port     = 27017
+      protocol    = "tcp"
+      cidr_block  = "0.0.0.0/0"
     },
   }
   intra_outbound_acl_rules = {
-    "mongodb" = {
-      "rule_number" = 110
-      "rule_action" = "allow"
-      "from_port"   = 27017
-      "to_port"     = 27017
-      "protocol"    = "tcp"
-      "cidr_block"  = "0.0.0.0/0"
+    mongodb = {
+      rule_number = 110
+      rule_action = "allow"
+      from_port   = 27017
+      to_port     = 27017
+      protocol    = "tcp"
+      cidr_block  = "0.0.0.0/0"
     },
   }
   # Security groups
   intra_inbound_sg_rules = {
-    "ssh-tcp" = {
-      "from_port" = 22
-      "to_port"   = 22
-      "protocol"  = "tcp"
+    ssh-tcp = {
+      from_port = 22
+      to_port   = 22
+      protocol  = "tcp"
     },
-    "mongodb-tcp" = {
-      "from_port" = 27017
-      "to_port"   = 27017
-      "protocol"  = "tcp"
+    mongodb-tcp = {
+      from_port = 27017
+      to_port   = 27017
+      protocol  = "tcp"
     },
   }
   intra_outbound_sg_rules = {
-    "ssh-tcp" = {
-      "from_port" = 22
-      "to_port"   = 22
-      "protocol"  = "tcp"
+    ssh-tcp = {
+      from_port = 22
+      to_port   = 22
+      protocol  = "tcp"
     },
-    "mongodb-tcp" = {
-      "from_port" = 27017
-      "to_port"   = 27017
-      "protocol"  = "tcp"
+    mongodb-tcp = {
+      from_port = 27017
+      to_port   = 27017
+      protocol  = "tcp"
     },
   }
 }
